b929089eab5158325675ed38f001e1721e6202de
nulln0ne <nulln0ne@protonmail.com>
2024-02-04 23:30:11 +0300

    update

--- config.def.h
+++ config.def.h
@@ -9,8 +9,8 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#839496", "#002b36" },
-	[SchemeSel] = { "#839496", "#073642" },
+	[SchemeNorm] = { "#586e75", "#002b36" },
+	[SchemeSel] = { "#839496", "#002b36" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
